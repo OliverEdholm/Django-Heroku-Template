9da0c84003bb415a91d0ca98e6bc2ea986bc3a30
OliverEdholm <oliver.edholm@gmail.com>
2018-06-08 15:52:19 +0200

    Initial commit

---
+++ backend/settings/testing.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+
+DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
